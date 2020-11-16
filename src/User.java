293249e10a8bdaec9e3ee15d559e3d1c84776c29
kmekdoud <karimmekdoud@gmail.com>
2020-11-16 04:11:00 -0500

    2 commit

--- src/User.java
+++ src/User.java
@@ -63,75 +63,96 @@ public class User {
         final String POrole="PO";
         boolean Login=true;
 
-        switch (getRole()){
+    switch (getRole()) {
 
-            case SMrole:
-                while(Login) {
-                    ScrumMaster scrumMaster = new ScrumMaster();
-                    System.out.println("1. Create BackLog");
-                    System.out.println("2. Count Requirements");
-                    System.out.println("3. Assign Requirements");
-                    System.out.println("4. Logout");
-                    System.out.println("Enter your choice :");
-                    Integer SMchoce = Integer.parseInt(br.readLine());
-                    if (SMchoce == 1) {
-                        System.out.println("Enter the number of requirement to create :");
-                        Integer nreqIT = Integer.parseInt(br.readLine());
-                        requirement = new Requirement[nreqIT];
-                        for (Integer i = 0; i < nreqIT; i++) {
-                            Integer j = i;
-                            System.out.println("Enter the id and description " + (j + 1) + ": ");
-                            String redetail = br.readLine();
-                            String detail = redetail.concat(",").concat(i.toString());
-                            scrumMaster.createRequirements(detail, requirement);
-                        }
-                        System.out.println("Requirement created successfully");
-                    } else if (SMchoce == 2) {
-                        System.out.println("The total number of requirement is : " + scrumMaster.countTotalRequirements(requirement));
-                    } else if (SMchoce == 3) {
-                        for (Requirement req : requirement){
-                            System.out.println(req.getId()+" "+req.getDescription());
-                        }
-                        System.out.println("Enter the requirement id and user name :");
-                        scrumMaster.assignreqmts(br, requirement, user);
-                    } else if (SMchoce == 4) {
-                       }
-                      }
-                break;
-            case TMrole:
-                TeamMember teamMember=new TeamMember();
+        case SMrole:
+            while (Login){
+                ScrumMaster scrumMaster = new ScrumMaster();
+                System.out.println("1. Create BackLog");
+                System.out.println("2. Count Requirements");
+                System.out.println("3. Assign Requirements");
+                System.out.println("4. Logout");
+                System.out.println("Enter your choice :");
+                Integer SMchoce = Integer.parseInt(br.readLine());
+                if (SMchoce == 1) {
+                    System.out.println("Enter the number of requirement to create :");
+                    Integer nreqIT = Integer.parseInt(br.readLine());
+                    requirement = new Requirement[nreqIT];
+                    for (Integer i = 0; i < nreqIT; i++) {
+                        Integer j = i;
+                        System.out.println("Enter the id and description " + (j + 1) + ": ");
+                        String redetail = br.readLine();
+                        String detail = redetail.concat(",").concat(i.toString());
+                        scrumMaster.createRequirements(detail, requirement);
+                    }
+                    for(Requirement RC :requirement){
+                        System.out.println(RC.getId()+" , "+RC.getDescription()+" , "+RC.getAssignedTo());
+                    }
+                    System.out.println("Requirement created successfully");
+                  }
+                else if (SMchoce == 2) {
+                    System.out.println("The total number of requirement is : " + scrumMaster.countTotalRequirements(requirement));
+                  }
+               else if (SMchoce == 3) {
+                    for (Requirement req : requirement) {
+                        System.out.println(req.getId() + " " + req.getDescription());
+                    }
+                    System.out.println("Enter the requirement id and user name :");
+                    scrumMaster.assignreqmts(br, requirement, user);
+                    for(Requirement RC :requirement){
+                        System.out.println(RC.getId()+" , "+RC.getDescription()+" , "+RC.getAssignedTo());
+                    }
+                   }
+               else  if (SMchoce == 4) {Login=false; }
+                }
+               break;
+
+
+
+        case TMrole:
+                    while (Login){
+                TeamMember teamMember = new TeamMember();
                 System.out.println("1. Display requirement list");
                 System.out.println("2. Logout");
                 System.out.println("Enter your choice :");
-                br.readLine();
-                if(Integer.parseInt(br.readLine())==1){teamMember.myRequriements(requirement,userName);}
-                else if(Integer.parseInt(br.readLine())==2){}
+                Integer TMchoce=Integer.parseInt(br.readLine());
+                if (TMchoce == 1) {
 
-                break;
-            case POrole:
+                           String usName = getUserName();
+                            teamMember.myRequriements(requirement,usName);
+
+
+                }
+                else if (TMchoce == 2) {Login=false; }
+                    }
+               break;
+
+        case POrole:
+
                 ProductOwner productOwner = new ProductOwner();
                 System.out.println("1. List requirements");
                 System.out.println("2. Allocate budget");
                 System.out.println("3. Allocate plan time");
                 System.out.println("4. Logout");
-                br.readLine();
-                if(Integer.parseInt(br.readLine())==1){
-                    System.out.format("%-15s %-15s %-15s %s\n", "Id","Description","Budget","Time");
-                    for(Requirement rc : requirement){
-                    System.out.format("%-15s %-15s %-15s %s\n", rc.getId(),rc.getDescription(),rc.getBudget(),rc.getPlanTime());
-                   }
-                }
-                else if(Integer.parseInt(br.readLine())==2){
+                Integer POchose=Integer.parseInt(br.readLine());
+                if (POchose == 1) {
+                    System.out.format("%-15s %-15s %-15s %s\n", "Id", "Description", "Budget", "Time");
+                    for (Requirement rc : requirement) {
+                        System.out.format("%-15s %-15s %-15s %s\n", rc.getId(), rc.getDescription(), rc.getBudget(), rc.getPlanTime());
+                    }
+                } else if (POchose == 2) {
                     System.out.println("Enter the id and budget :");
-                    String budgetDetail= br.readLine();
-                    productOwner.allocateBudget(budgetDetail, requirement);}
-                else if(Integer.parseInt(br.readLine())==3){
+                    String budgetDetail = br.readLine();
+                    productOwner.allocateBudget(budgetDetail, requirement);
+                } else if (POchose == 3) {
                     System.out.println("Enter the id and plan time :");
-                    String planTimeDetail= br.readLine();
-                    productOwner.allocatePlanTime(planTimeDetail,requirement);}
-                else if(Integer.parseInt(br.readLine())==4){break;}
+                    String planTimeDetail = br.readLine();
+                    productOwner.allocatePlanTime(planTimeDetail, requirement);
+                } else if (POchose == 4) {Login=false; }
+                break;
 
-        }
+
+    }
 
     }
 }
